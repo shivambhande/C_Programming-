8838781f7c2c63a8c684fca5c82417606f079ba0
shivambhande <103504392+shivambhande@users.noreply.github.com>
2022-09-20 17:03:40 +0530

    Add files via upload

---
+++ First_c_program.c
@@ -0,0 +1,26 @@
+
+////////////////////////////////////////////////////////////////////
+//
+//  @ Author : Shivam Bhande
+//  @ Goal   : To print Jay Ganesh ...   On screen 
+//  @ Date   : 20 / 09 /2022
+//
+////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+int main()
+{
+	printf("Jay Ganesh...\n");
+
+	return 0;
+}
+
+// gcc program1.c  -o  Myexe
+
+// Windows
+// Myexe
+
+// Linux / MacOS
+// ./Myexe
